6ecc0be220d8153859c53455d6600c6e4f692da5
Valery Briz <valerybriz@gmail.com>
2020-01-19 11:07:36 -0600

    initializing debugging and virtual environments

---
+++ debugging_scripts.py
@@ -0,0 +1,10 @@
+def funcA(first_val, second_val):
+    result = (first_val*2) - (second_val/4)
+    return result
+
+def functionB(first_val=23, last_val=72):
+    response = funcA(first_val, last_vale)
+    result = response * first_val / 7
+    return result
+    
+functionB(33,88) # we are evaluating the funciton.
